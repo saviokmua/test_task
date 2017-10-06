47fded3b1da641edc3f33301cef7b8bd683b1ffd
Olexandr Matkovsky <savio.km.ua@gmail.com>
2017-10-06 07:08:08 +0300

    added Categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ApplicationRecord
+  has_and_belongs_to_many :categories
   has_attached_file :file,    styles: { medium: "500x500>", thumb: "250x250>" }
   validates_attachment_content_type :file, 
     content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
     validates_attachment_size :file, :in => 0.megabytes..2.megabytes
-  end
+  validates :name, :content, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006030950) do
+ActiveRecord::Schema.define(version: 20171006035947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_posts", id: false, force: :cascade do |t|
+    t.bigint "category_id"
+    t.bigint "post_id"
+    t.index ["category_id"], name: "index_categories_posts_on_category_id"
+    t.index ["post_id"], name: "index_categories_posts_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
-    t.text "name"
+    t.string "name"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
