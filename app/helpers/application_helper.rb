edb2e9498906b9add32833119714fc6fa4f6b75c
Olexandr Matkovsky <savio.km.ua@gmail.com>
2017-10-07 06:47:21 +0300

    added active state for menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def root_active
+    "active" if current_page?(root_path)
+  end
+
+  def page_active(name)
+    "active" if controller_name == name and !current_page?(root_path)
+  end
 end
